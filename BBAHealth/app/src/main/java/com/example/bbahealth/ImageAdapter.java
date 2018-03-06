4fec01b95fb3ccd5836c1cf7fc9b13ffa6f41dd1
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2018-03-06 12:41:43 +0000

    Update

---
+++ BBAHealth/app/src/main/java/com/example/bbahealth/ImageAdapter.java
@@ -0,0 +1,49 @@
+package com.example.bbahealth;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter{
+
+    private Context context;
+
+    public ImageAdapter(Context c, int i) {
+        context = c;
+    }
+
+    public int getCount() {
+        return ThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(ThumbIds[position]);
+        return imageView;
+    }
+
+    private Integer[] ThumbIds = {
+            R.drawable.ic_card_image_one
+    };
+}
--- BBAHealth/app/src/main/java/com/example/bbahealth/InfoFragment.java
+++ BBAHealth/app/src/main/java/com/example/bbahealth/InfoFragment.java
@@ -1,7 +1,5 @@
 package com.example.bbahealth;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,6 +13,7 @@ public class InfoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_info, container, false);
+
     }
 
 }
