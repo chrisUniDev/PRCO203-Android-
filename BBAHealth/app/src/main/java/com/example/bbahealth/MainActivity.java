884325cce22e97d91ceb959ea8d1f82ac4f56556
Harrison Bennett <32133987+HMSBennett@users.noreply.github.com>
2018-03-19 19:48:03 +0000

    Added Weather API, needs modification and correction

--- BBAHealth/app/src/main/java/com/example/bbahealth/MainActivity.java
+++ BBAHealth/app/src/main/java/com/example/bbahealth/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bbahealth;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,8 +10,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    InfoFragment infoFragment = new InfoFragment();
+    MapsFragment mapsFragment = new MapsFragment();
+    RecordFragment recordFragment = new RecordFragment();
+    TrackerFragment trackerFragment = new TrackerFragment();
+    SupportFragment supportFragment = new SupportFragment();
+    OldRecordingFragment oldRecordingFragment = new OldRecordingFragment();
+    NewRecordingFragment newRecordingFragment = new NewRecordingFragment();
+    InfoOpenFragment infoOpenFragment = new InfoOpenFragment();
+
+    public boolean infoViewOpen = false;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -19,49 +32,48 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     setTitle("BBA Health");
-                    InfoFragment infoFragment = new InfoFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction1.replace(R.id.fragmentScreen, infoFragment, "FragName");
                     fragmentTransaction1.commit();
+                    infoViewOpen = true;
                     return true;
                 case R.id.navigation_maps:
                     setTitle("Maps");
-                    MapsFragment mapsFragment = new MapsFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction2.replace(R.id.fragmentScreen, mapsFragment, "FragName");
                     fragmentTransaction2.commit();
                     getSupportFragmentManager().beginTransaction().add(R.id.fragmentScreen, new MapsFragment()).commit();
+                    infoViewOpen = false;
                     return true;
                 case R.id.navigation_record:
                     setTitle("Record");
-                    RecordFragment recordFragment = new RecordFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction3.replace(R.id.fragmentScreen, recordFragment, "FragName");
                     fragmentTransaction3.commit();
                     getSupportFragmentManager().beginTransaction().add(R.id.fragmentScreen, new RecordFragment()).commit();
 
-                    NewRecordingFragment newRecordingFragment = new NewRecordingFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction.replace(R.id.recordFragmentScreen, newRecordingFragment, "FragName");
                     fragmentTransaction.commit();
 
                     temp = false;
+                    infoViewOpen = false;
                     return true;
                 case R.id.navigation_tracker:
                     setTitle("Tracker");
-                    TrackerFragment trackerFragment = new TrackerFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction4.replace(R.id.fragmentScreen, trackerFragment, "FragName");
                     fragmentTransaction4.commit();
                     getSupportFragmentManager().beginTransaction().add(R.id.fragmentScreen, new TrackerFragment()).commit();
+                    infoViewOpen = false;
                     return true;
                 case R.id.navigation_support:
                     setTitle("Support");
-                    SupportFragment supportFragment = new SupportFragment();
                     android.support.v4.app.FragmentTransaction fragmentTransaction5 = getSupportFragmentManager().beginTransaction();
                     fragmentTransaction5.replace(R.id.fragmentScreen, supportFragment, "FragName");
                     fragmentTransaction5.commit();
                     getSupportFragmentManager().beginTransaction().add(R.id.fragmentScreen, new SupportFragment()).commit();
+                    infoViewOpen = false;
                     return true;
             }
             return false;
@@ -77,19 +89,24 @@ public class MainActivity extends AppCompatActivity {
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragmentScreen, infoFragment, "FragName");
         fragmentTransaction.commit();
+        infoViewOpen = true;
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         NavBarHelper.disableShiftMode(navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        GridView gridView = (GridView) findViewById(R.id.gridview);
+        GridView gridView = findViewById(R.id.gridViewCards);
         gridView.setAdapter(new ImageAdapter(this));
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                openInfoView();
-                //Toast.makeText(MainActivity.this, "" + i, Toast.LENGTH_LONG).show();
+                if(infoViewOpen){
+                    openInfoView();
+                    Toast.makeText(MainActivity.this, "Card: " + i, Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
             }
         });
 
@@ -101,6 +118,15 @@ public class MainActivity extends AppCompatActivity {
         //    }
         //});
     }
+
+    public void openInfoView(){
+        setTitle("Test");
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentScreen, infoOpenFragment, "FragName");
+        fragmentTransaction.commit();
+    }
+
+    //Recording Area
 
     boolean temp = false;
 
@@ -116,7 +142,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeViewOld(){
         setTitle("Old Recordings");
-        OldRecordingFragment oldRecordingFragment = new OldRecordingFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.recordFragmentScreen, oldRecordingFragment, "FragName");
         fragmentTransaction.commit();
@@ -124,18 +149,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void changeViewNew(){
         setTitle("Record");
-        NewRecordingFragment newRecordingFragment = new NewRecordingFragment();
         android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.recordFragmentScreen, newRecordingFragment, "FragName");
         fragmentTransaction.commit();
     }
 
-    public void openInfoView(){
-        setTitle("Test");
-        InfoOpenFragment infoOpenFragment = new InfoOpenFragment();
-        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentScreen, infoOpenFragment, "FragName");
-        fragmentTransaction.commit();
+    protected boolean hasMicrophone(){
+        PackageManager packageManager = this.getPackageManager();
+        return packageManager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
     }
 
     public void record(View view){
