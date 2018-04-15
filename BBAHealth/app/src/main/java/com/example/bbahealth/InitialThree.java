f24fb3a2bbca1cb587a6bcb458652ed38b8ca25e
Ross Goold <rossgoold@hotmail.co.uk>
2018-04-15 15:01:37 +0100

    Initialisation Setup Progress

--- BBAHealth/app/src/main/java/com/example/bbahealth/InitialThree.java
+++ BBAHealth/app/src/main/java/com/example/bbahealth/InitialThree.java
@@ -6,12 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class InitialThree extends Fragment {
-
-    View view;
 
     public InitialThree() {
         // Required empty public constructor
@@ -21,26 +22,64 @@ public class InitialThree extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_initial_three, container, false);
 
-        Button buttonClose = (Button) view.findViewById(R.id.buttonClose);
+        // needs to be inflated first
+        View view = inflater.inflate(R.layout.fragment_initial_three, container, false);
 
-        buttonClose.setOnClickListener(new View.OnClickListener() {
+        String[] cancerOptions = {"Bladder", "Breast", "Gynae", "Head & Neck", "Lower GI", "Prostate", "Skin", "Upper GI"};
+
+        ListView listView = (ListView) view.findViewById(R.id.listViewCancerSelection);
+
+        // figuring out what to draw on screen
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, cancerOptions );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) throws IllegalArgumentException,
-                    SecurityException, IllegalStateException {
-
-                getActivity().finish();
-
-                MainActivity mainActivity = new MainActivity();
-                mainActivity.firstTime = true;
-
-                Toast.makeText(getActivity().getApplicationContext(), "Initialization Closed" ,Toast.LENGTH_LONG).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0)
+                {
+                    Toast.makeText(getActivity(), "You have chosen bladder cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 1)
+                {
+                    Toast.makeText(getActivity(), "You have chosen breast cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 2)
+                {
+                    Toast.makeText(getActivity(), "You have chosen gynae cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 3)
+                {
+                    Toast.makeText(getActivity(), "You have chosen head & neck cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 4)
+                {
+                    Toast.makeText(getActivity(), "You have chosen lower GI cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 5)
+                {
+                    Toast.makeText(getActivity(), "You have chosen prostate cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 6)
+                {
+                    Toast.makeText(getActivity(), "You have chosen skin cancer", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 7)
+                {
+                    Toast.makeText(getActivity(), "You have chosen upper GI cancer", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+
+
+        // Inflate the layout for this fragment
         return view;
+
+
+
     }
 
 }
--- BBAHealth/app/src/main/java/com/example/bbahealth/InitialTwo.java
+++ BBAHealth/app/src/main/java/com/example/bbahealth/InitialTwo.java
@@ -1,13 +1,20 @@
 package com.example.bbahealth;
 
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class InitialTwo extends Fragment {
+
+
 
 
     public InitialTwo() {
@@ -18,8 +25,40 @@ public class InitialTwo extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // needs to be inflated first
+        View view = inflater.inflate(R.layout.fragment_initial_two, container, false);
+
+        String[] pathwayOptions = {"Palliative", "Radical"};
+
+        ListView listView = (ListView) view.findViewById(R.id.listViewPathwayOptions);
+
+        // figuring out what to draw on screen
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, pathwayOptions );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0)
+                {
+                    Toast.makeText(getActivity(), "You have chosen Palliative pathway", Toast.LENGTH_SHORT).show();
+                }
+                else if (position == 1)
+                {
+                    Toast.makeText(getActivity(), "You have chosen Radical pathway", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_two, container, false);
+        return view;
+
+
+
     }
+
+
 
 }
